58c54bd13cc081f1b68b641923e1f908ff42ea81
Alex Bikfalvi <alex@bikfalvi.com>
2014-01-30 00:38:45 +0100

    Added Internet Analytics project.
    Changed fonts/style.

--- script/ga.js
+++ script/ga.js
@@ -1,4 +1,5 @@
 // Google Analytics script
+
 /*
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -7,16 +8,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-24480564-1', 'bikfalvi.com');
 ga('send', 'pageview');
-
-/*
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-24480564-1']);
-_gaq.push(['_setDomainName', '.bikfalvi.com']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 */
